ecef804841bf86e752ec03a4366673ed37cd8808
nitin-ivn <ivnsainitin@gmail.com>
2024-11-30 20:03:31 +0530

    homepage

--- code-editor/app/page.tsx
+++ code-editor/app/page.tsx
@@ -3,9 +3,9 @@
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 export default function Home() {
-  // useEffect(() => {
-  //   redirect('/editor/javascript')
-  // })
+  useEffect(() => {
+    redirect('/editor/javascript')
+  })
   return (
     <div className="page">
         <h1 style={{fontSize: "3vw"}}>Welcome to Online Code Editor</h1>
