371d8bdb585a0a53eb382bbadf1a9abf40e3426d
Sai Krishna <ivnsainitin@gmail.com>
2024-11-30 11:41:57 +0530

    style for sidebar

--- code-editor/app/Sidebar.tsx
+++ code-editor/app/Sidebar.tsx
@@ -1,46 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
 
-const sidebar = () => {
+import Link from 'next/link';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+
+const Sidebar = () => {
+  const path = usePathname();
+  const Activelanguage: string = path.split('/')[2];
+  const isActive = (language: string) => Activelanguage === language
+
+
   return (
     <div className='flex flex-col sidebar'>
-      <div className='lang mt-4'>
-        <Link href='/editor/javascript'>
-          <img src="/icons/js.png" alt="" />
-        </Link>
-        </div>
-      <div className='lang'>
-        <Link href='/editor/python'>
-          <img src="/icons/python.png" alt="" />
-        </Link>
-      </div>
-      <div className="lang">
-        <Link href='/editor/go'>
-          <img src="/icons/go.webp" alt="" />
-        </Link>
-      </div>
-      <div className="lang">
-      <Link href='/editor/php'>
-        <img src="/icons/php.png" alt="" />
+      <Link href='/editor/javascript' className={`lang mt-4 ${isActive('javascript') ? 'lang-active' : ''}`}>
+          <img src='/icons/js.png' alt='JavaScript Icon' />
       </Link>
-      </div>
-      <div className="lang">
-      <Link href='/editor/swift'>
-        <img src="/icons/swift.png" alt="" />
+      <Link href='/editor/python' className={`lang ${isActive('python') ? 'lang-active' : ''}`}>
+          <img src='/icons/python.png' alt='Python Icon' />
       </Link>
-      </div>
-      <div className="lang">
-      <Link href='/editor/rust'>
-        <img src="/icons/rust.png" alt="" />
+      <Link href='/editor/go' className={`lang ${isActive('go') ? 'lang-active' : ''}`}>
+          <img src='/icons/go.webp' alt='Go Icon' />
       </Link>
-      </div>
-      <div className="lang">
-      <Link href='/editor/c'>
-        <img src="/icons/C.png" alt="" />
+      <Link href='/editor/php' className={`lang ${isActive('php') ? 'lang-active' : ''}`}>
+          <img src='/icons/php.png' alt='PHP Icon' />
       </Link>
-      </div>
+      <Link href='/editor/swift' className={`lang ${isActive('swift') ? 'lang-active' : ''}`}>
+          <img src='/icons/swift.png' alt='Swift Icon' />
+      </Link>
+      <Link href='/editor/rust' className={`lang ${isActive('rust') ? 'lang-active' : ''}`}>
+          <img src='/icons/rust.png' alt='Rust Icon' />
+      </Link>
+      <Link href='/editor/c' className={`lang ${isActive('c') ? 'lang-active' : ''}`}>
+          <img src='/icons/C.png' alt='C Icon' />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default sidebar
+export default Sidebar;
